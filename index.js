c511a6ec814fbd2cdbc2f55c99a2693e8d8462ea
AllenCodess <Aph296@protonmail.com>
2026-01-29 15:53:34 -0500

    completed the calculator app

---
+++ index.js
@@ -0,0 +1,51 @@
+
+
+//Variables
+const numberOne = 9
+const numberTwo = 8
+const answerDisplay = document.querySelector('.answer')
+const additionBtn = document.getElementById('add-btn')
+const subtractionBtn = document.getElementById('sub-btn')
+const multiplyBtn = document.getElementById('multiply-btn')
+const divideBtn = document.getElementById('divide-btn')
+const clearBtn = document.querySelector(".clear")
+
+
+//EventListeners
+additionBtn.addEventListener("click", addNumbers)
+subtractionBtn.addEventListener("click" , subtractNumbers)
+multiplyBtn.addEventListener("click", multiplyNumbers)
+divideBtn.addEventListener("click" , divideNumbers)
+clearBtn.addEventListener("click", clear)
+
+
+//functions 
+
+function addNumbers() {
+   sum =  numberOne + numberTwo
+   answer()
+}
+
+function subtractNumbers() {
+    sum = numberOne - numberTwo
+    answer()
+}
+
+function multiplyNumbers () {
+    sum = numberOne * numberTwo
+    answer()
+}
+
+function divideNumbers (){
+    sum = numberOne / numberTwo
+    answer()
+}
+
+function answer() {
+    answerDisplay.textContent += sum
+}
+
+
+function clear() {
+    answerDisplay.textContent = 'Answer: '
+}
